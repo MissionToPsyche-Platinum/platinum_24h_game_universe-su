9db80c96105eb45e08b4148b22d95d6d59277be8
ChocolateMilk71 <olliepurdy@comcast.net>
2025-11-24 13:53:15 -0800

    Refactored ShipBuildingGrid.cs to no longer poll for left mouse click. Now listens for the leftMouseClick event.

--- Assets/Scripts/ShipBuildingGrid.cs
+++ Assets/Scripts/ShipBuildingGrid.cs
@@ -16,27 +16,16 @@ public class ShipBuildingGrid : MonoBehaviour {
     private bool someTileSelected = false;
     private void Awake() => grid = new Grid(gridWidth, gridHeight, cellSize, gridOriginPosition);
     
-    private void Start() => gameInput.OnNumKeyPerformedAction += GameInput_OnNumKeyAction;
-    
-    private void Update() {
-        if (Input.GetMouseButtonDown(0)) { //Left click
-            //grid.SetValue(Mouse.GetMouseWorldPosition(), 56);
-            Debug.Log("check1");
-            (int, int) clickCoords;
-            grid.GetXY(Mouse.GetMouseWorldPosition(), out clickCoords.Item1, out clickCoords.Item2);
+    private void Start() {
+        gameInput.OnNumKeyPerformedAction += GameInput_OnNumKeyAction;
+        gameInput.OnLeftMouseClickPerformedAction += GameInput_OnLeftMouseClickAction;
+    }
 
-            if (clickCoords.Item1 < 0 || clickCoords.Item2 < 0 ||
-                clickCoords.Item1 >= gridWidth || clickCoords.Item2 >= gridHeight) {
-                
-                someTileSelected = false;
-                return;
-            }
+    private Vector3 GridCoordinatesToUnityPosition((int, int) gridCoords) {
+        float x = gridOriginPosition.x + cellSize / 2 + (cellSize * gridCoords.Item1);
+        float y = gridOriginPosition.y + cellSize / 2 +(cellSize * gridCoords.Item2);
 
-            someTileSelected = true;
-            selectedTileCoords = clickCoords;
-        }
-        
-        
+        return new Vector3(x, y);
     }
     
     private void GameInput_OnNumKeyAction(object sender, GameInput.NumKeyEventArgs e) {
@@ -47,12 +36,20 @@ public class ShipBuildingGrid : MonoBehaviour {
         spacecraftPart.SetActive(true);
         spacecraftPart.transform.position = GridCoordinatesToUnityPosition(selectedTileCoords);
     }
+    
+    private void GameInput_OnLeftMouseClickAction(object sender, System.EventArgs e) {
+        (int, int) clickCoords;
+        grid.GetXY(Mouse.GetMouseWorldPosition(), out clickCoords.Item1, out clickCoords.Item2);
 
-    private Vector3 GridCoordinatesToUnityPosition((int, int) gridCoords) {
-        float x = gridOriginPosition.x + cellSize / 2 + (cellSize * gridCoords.Item1);
-        float y = gridOriginPosition.y + cellSize / 2 +(cellSize * gridCoords.Item2);
+        if (clickCoords.Item1 < 0 || clickCoords.Item2 < 0 ||
+            clickCoords.Item1 >= gridWidth || clickCoords.Item2 >= gridHeight) {
+                
+            someTileSelected = false;
+            return;
+        }
 
-        return new Vector3(x, y);
+        someTileSelected = true;
+        selectedTileCoords = clickCoords;
     }
     
 }
