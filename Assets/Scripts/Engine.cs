35630fd3e62e0f7661411b122f58c2473053d36b
ChocolateMilk71 <olliepurdy@comcast.net>
2025-11-24 13:47:37 -0800

    Refactored engine.cs to conform with coding standards

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -5,12 +5,8 @@ public class Engine : MonoBehaviour {
     [SerializeField] private Spacecraft spacecraft;
     [SerializeField] private int speed;
 
-    public void Awake() {
-        enabled = false;
-    }
-    private void Update() {
-        spacecraft.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * speed;
-    }
+    public void Awake() => enabled = false;
+    private void Update() => spacecraft.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * speed;
 
 
 
