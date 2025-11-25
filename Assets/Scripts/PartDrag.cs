c8782a9be331eabab828d3c6c9d002e71c06ff67
muratozcubukcu <murat.ozcubukcu@gmail.com>
2025-11-25 01:11:11 -0800

    Update PartDrag to snap parts to ship surface and lock rotation

--- Assets/Scripts/PartDrag.cs
+++ Assets/Scripts/PartDrag.cs
@@ -7,32 +7,49 @@ public class PartDrag : MonoBehaviour
 
 private Vector3 screenPoint;
 private Vector3 offset;
+private BoxCollider2D partCollider;
+private Quaternion lockedRotation;
 
-    /// <summary>
-    /// When clicked, the object determines its relative position to the mouse for later use in onmousedrag
-    /// </summary>
+    private void Awake() {
+        partCollider = GetComponent<BoxCollider2D>();
+        lockedRotation = transform.rotation;
+    }
+
     void OnMouseDown() {
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
-    /// <summary>
-    /// When dragged, the object changes its position to follow the mouse
-    /// </summary>
     void OnMouseDrag() {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
+        transform.rotation = lockedRotation;
     }
 
-    /// <summary>
-    /// when let go, the object snaps to a grid space if it is in one
-    /// </summary>
     void OnMouseUp()
     {
+        transform.rotation = lockedRotation;
+        
+        if (ShipBuildingGrid.instance == null || partCollider == null) return;
+        
+        Vector3? shipSnapPosition = ShipBuildingGrid.instance.SnapToShipSurface(transform.position, partCollider);
+        
+        if (shipSnapPosition != null) {
+            transform.position = (Vector3)shipSnapPosition;
+            ShipBuildingGrid.instance.AttachPartToShip(gameObject);
+            return;
+        }
+        
         Vector3? gridSnapPosition = ShipBuildingGrid.instance.PostionToGridPosition(transform.position);
         if (gridSnapPosition != null) {
-            transform.position = (Vector3) gridSnapPosition;
+            transform.position = (Vector3)gridSnapPosition;
+        }
+    }
+    
+    private void Update() {
+        if (transform.rotation != lockedRotation) {
+            transform.rotation = lockedRotation;
         }
     }
 }
