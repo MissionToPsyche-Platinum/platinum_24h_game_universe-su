cda3644cc7df11aaf955d8a30c5262c7e74bcce1
bananacat50 <elllliotttt@gmail.com>
2025-11-25 19:14:26 -0800

    Added comments to GridVisualizer.cs

--- Assets/Scripts/GridVisualizer.cs
+++ Assets/Scripts/GridVisualizer.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// A container class for Grid that translates it into sprites so that it is visible without gizmos
+/// </summary>
 public class GridVisualizer : MonoBehaviour {
     
     [SerializeField] private Grid grid;
@@ -14,6 +17,9 @@ public class GridVisualizer : MonoBehaviour {
         CreateLineSprite();
     }
     
+    /// <summary>
+    /// creates the sprite that is stretched to create the lines of the grid
+    /// </summary>
     private void CreateLineSprite() {
         Texture2D texture = new Texture2D(1, 1);
         texture.SetPixel(0, 0, Color.white);
@@ -21,14 +27,25 @@ public class GridVisualizer : MonoBehaviour {
         lineSprite = Sprite.Create(texture, new Rect(0, 0, 1, 1), new Vector2(0.5f, 0.5f), 1f);
     }
     
+    /// <summary>
+    /// draws the grid as a set of stretched sprites
+    /// </summary>
+    /// <param name="grid">The grid to be visualized
+    /// (note: this parameter should be removed and the grid should be set at construction)</param>
+    /// <param name="width">the width of the grid in tiles</param>
+    /// <param name="height">the height of the grid in tiles</param>
+    /// <param name="cellSize">the width and height of the tiles in game units</param>
+    /// <param name="originPosition">the lower left corner of the grid</param>
     public void VisualizeGrid(Grid grid, int width, int height, float cellSize, Vector3 originPosition) {
         this.grid = grid;
         
+        //erase previous drawings of the grid
         foreach (var line in gridLines) {
             if (line != null) Destroy(line);
         }
         gridLines.Clear();
         
+        //draw vertical lines
         for (int x = 0; x <= width; x++) {
             GameObject lineObj = new GameObject("GridLine_V_" + x);
             lineObj.transform.SetParent(transform);
@@ -45,6 +62,7 @@ public class GridVisualizer : MonoBehaviour {
             gridLines.Add(lineObj);
         }
         
+        //draw horizontal lines
         for (int y = 0; y <= height; y++) {
             GameObject lineObj = new GameObject("GridLine_H_" + y);
             lineObj.transform.SetParent(transform);
