d24580bd161fef0b8ee54b9c8e866aa16d5e2184
ChocolateMilk71 <olliepurdy@comcast.net>
2025-12-27 00:09:22 -0800

    Refactored how to check if ship part can be placed in ShipBuildingGrid.cs
    Now utilizes the 2D array stored in the grid to check if there are parts in adjacent cells that can be connected with.

--- Assets/Scripts/ShipBuildingGrid.cs
+++ Assets/Scripts/ShipBuildingGrid.cs
@@ -3,32 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipBuildingGrid : MonoBehaviour {
-    
     public static ShipBuildingGrid instance {get; private set;}
+    
     [SerializeField] private GameInput gameInput;
-    [SerializeField] private GameObject[] spacecraftParts;
-    [SerializeField] private GameObject spaceshipPrefab;
+    [SerializeField] private SpacecraftPartDatabase partDatabase;
+    [SerializeField] private GameObject spacecraftPrefab;
     [SerializeField] private GridVisualizer gridVisualizer;
     
     private Grid grid;
     private int gridWidth = 5;
     private int gridHeight = 7;
     private float cellSize = 1f;
-    private Vector3 gridOriginPosition = new Vector3(-2.5f, -4f);
+    private Vector3 gridOriginPosition = new(-2.5f, -4f);
     
     private (int, int) selectedTileCoords;
     private bool someTileSelected = false;
     
-    private GameObject spaceship;
-    private BoxCollider2D spaceshipCollider;
-    private SpriteRenderer spaceshipSpriteRenderer;
-    private List<GameObject> attachedParts = new List<GameObject>();
-    private List<Transform> shipSnapPoints = new List<Transform>();
-    
-    private void Awake()
-    {
+    private void Awake() {
         instance = this;
         grid = new Grid(gridWidth, gridHeight, cellSize, gridOriginPosition);
+
+        CreateSpacecraft();
+        gridVisualizer.VisualizeGrid(grid, gridWidth, gridHeight, cellSize, gridOriginPosition);
     } 
     
     private void Start() {
@@ -41,83 +37,47 @@ public class ShipBuildingGrid : MonoBehaviour {
         spacecraftPrefab.transform.position = GridCoordinatesToUnityPosition(gridWidth / 2, gridHeight / 2);
         grid.SetValue(gridWidth / 2, gridHeight / 2, partDatabase.GetPartID(spacecraftPrefab));
     }
-    
-    private void CreateShipSnapPoints() {
-        foreach (var snapPoint in shipSnapPoints) {
-            if (snapPoint != null) Destroy(snapPoint.gameObject);
-        }
-        shipSnapPoints.Clear();
-        
-        if (spaceship == null) return;
-        
-        Bounds shipBounds;
-        if (spaceshipSpriteRenderer != null) {
-            shipBounds = spaceshipSpriteRenderer.bounds;
-        } else if (spaceshipCollider != null) {
-            shipBounds = spaceshipCollider.bounds;
-        } else {
-            return;
-        }
-        
-        Vector3 shipCenter = spaceship.transform.position;
-        float shipHalfWidth = shipBounds.extents.x;
-        float shipHalfHeight = shipBounds.extents.y;
-        
-        int pointsPerSide = 5;
-        for (int i = 0; i < pointsPerSide; i++) {
-            float t = (float)i / (pointsPerSide - 1);
-            float x = shipCenter.x - shipHalfWidth + (shipHalfWidth * 2f * t);
-            CreateSnapPoint(new Vector3(x, shipCenter.y + shipHalfHeight, 0), "Top_" + i);
-        }
-        
-        for (int i = 0; i < pointsPerSide; i++) {
-            float t = (float)i / (pointsPerSide - 1);
-            float x = shipCenter.x - shipHalfWidth + (shipHalfWidth * 2f * t);
-            CreateSnapPoint(new Vector3(x, shipCenter.y - shipHalfHeight, 0), "Bottom_" + i);
-        }
-        
-        for (int i = 0; i < pointsPerSide; i++) {
-            float t = (float)i / (pointsPerSide - 1);
-            float y = shipCenter.y - shipHalfHeight + (shipHalfHeight * 2f * t);
-            CreateSnapPoint(new Vector3(shipCenter.x - shipHalfWidth, y, 0), "Left_" + i);
-        }
-        
-        for (int i = 0; i < pointsPerSide; i++) {
-            float t = (float)i / (pointsPerSide - 1);
-            float y = shipCenter.y - shipHalfHeight + (shipHalfHeight * 2f * t);
-            CreateSnapPoint(new Vector3(shipCenter.x + shipHalfWidth, y, 0), "Right_" + i);
-        }
-    }
-    
-    private void CreateSnapPoint(Vector3 position, string name) {
-        GameObject snapPoint = new GameObject("SnapPoint_" + name);
-        snapPoint.transform.SetParent(spaceship.transform);
-        snapPoint.transform.position = position;
-        shipSnapPoints.Add(snapPoint.transform);
-    }
-    
-    private Sprite CreateRectangleSprite() {
-        Texture2D texture = new Texture2D(100, 100);
-        Color[] pixels = new Color[100 * 100];
-        for (int i = 0; i < pixels.Length; i++) {
-            pixels[i] = Color.white;
-        }
-        texture.SetPixels(pixels);
-        texture.Apply();
-        return Sprite.Create(texture, new Rect(0, 0, 100, 100), new Vector2(0.5f, 0.5f), 100f);
+
+    public void SetGridCellValue((int, int) coordinates, int value) {
+        grid.SetValue(coordinates.Item1, coordinates.Item2, value);
     }
 
+    public void SetGridCellValueByUnityPosition(Vector3 position, int value) {
+        (int, int) coordinates = UnityPositionToGridCoordinates(position);
+        
+        SetGridCellValue(coordinates, value);
+    }
+    
+    private Vector3 GridCoordinatesToUnityPosition(int x, int y) => GridCoordinatesToUnityPosition((x, y));
+    
+    
     private Vector3 GridCoordinatesToUnityPosition((int, int) gridCoords) {
         float x = gridOriginPosition.x + cellSize / 2 + (cellSize * gridCoords.Item1);
-        float y = gridOriginPosition.y + cellSize / 2 +(cellSize * gridCoords.Item2);
+        float y = gridOriginPosition.y + cellSize / 2 + (cellSize * gridCoords.Item2);
 
         return new Vector3(x, y);
     }
+
+    public (int, int) UnityPositionToGridCoordinates(Vector3 unityPosition) {
+        int x;
+        int y;
+        
+        grid.GetXY(unityPosition, out x, out y);
+
+        return (x, y);
+    }
     
     private void GameInput_OnNumKeyAction(object sender, GameInput.NumKeyEventArgs e) {
+        int x = selectedTileCoords.Item1;
+        int y = selectedTileCoords.Item2;
+        
         if (!someTileSelected) return;
-
-        GameObject spacecraftPart = Instantiate(spacecraftParts[e.key - 1]);
+        if (grid.GetValue(x, y) != -1) return; 
+        if (!CanPlacePart(partDatabase.GetPartGameObject(e.key), (x, y))) return;
+        
+        grid.SetValue(x, y, e.key);
+        
+        GameObject spacecraftPart = Instantiate(partDatabase.GetPartGameObject(e.key));
         
         spacecraftPart.SetActive(true);
         spacecraftPart.transform.position = GridCoordinatesToUnityPosition(selectedTileCoords);
@@ -133,92 +93,56 @@ public class ShipBuildingGrid : MonoBehaviour {
             someTileSelected = false;
             return;
         }
-
+        
         someTileSelected = true;
         selectedTileCoords = clickCoords;
     }
+
+    public bool CanPlacePart(GameObject partToBePlaced, (int, int) coords) {
+        List<string> possibleConnectionsOfPartToBePlaced = partDatabase.GetSnapableDirections(partToBePlaced);
+        int x = coords.Item1;
+        int y = coords.Item2;
+
+        foreach (string snapableDirection in possibleConnectionsOfPartToBePlaced) {
+            switch (snapableDirection) {
+                case "above":
+                    if (PartCanConnect(grid.GetValue(x, y + 1), "below")) return true;
+                    break;
+                case "below":
+                    if (PartCanConnect(grid.GetValue(x, y - 1), "above")) return true;
+                    break;
+                case "left":
+                    if (PartCanConnect(grid.GetValue(x - 1, y), "right")) return true;
+                    break;
+                case "right":
+                    if (PartCanConnect(grid.GetValue(x + 1, y), "left")) return true;
+                    break;
+                default:
+                    continue;
+            }
+        }
+        
+        return false;
+    }
+
+    private bool PartCanConnect(int partID, string connectingDirection) {
+        if (partID < 0) return false;
+        
+        List<string> snapableDirections = partDatabase.GetSnapableDirections(partID);
+
+        return snapableDirections.Contains(connectingDirection);
+    }
     
-    public Vector3? PostionToGridPosition(Vector3 originalPosition)
-    {
+    public Vector3? PostionToGridPosition(Vector3 originalPosition) {
         (int, int) tileCoords;
         grid.GetXY(originalPosition, out tileCoords.Item1, out tileCoords.Item2);
 
         if (tileCoords.Item1 < 0 || tileCoords.Item2 < 0 ||
             tileCoords.Item1 >= gridWidth || tileCoords.Item2 >= gridHeight) {
+            
             return null;
         }
+        
         return GridCoordinatesToUnityPosition(tileCoords);
-    }
-    
-    // Snaps part to ship surface using snap points
-    public Vector3? SnapToShipSurface(Vector3 partPosition, BoxCollider2D partCollider) {
-        if (spaceship == null || partCollider == null || shipSnapPoints.Count == 0) return null;
-        
-        List<Vector3> partSnapPoints = GetPartSnapPoints(partPosition, partCollider);
-        
-        float snapRange = 1.5f;
-        Transform closestShipSnapPoint = null;
-        Vector3 closestPartSnapPoint = Vector3.zero;
-        float closestDistance = float.MaxValue;
-        
-        foreach (Transform shipSnapPoint in shipSnapPoints) {
-            foreach (Vector3 partSnapPoint in partSnapPoints) {
-                float distance = Vector3.Distance(shipSnapPoint.position, partSnapPoint);
-                if (distance < closestDistance && distance <= snapRange) {
-                    closestDistance = distance;
-                    closestShipSnapPoint = shipSnapPoint;
-                    closestPartSnapPoint = partSnapPoint;
-                }
-            }
-        }
-        
-        if (closestShipSnapPoint != null) {
-            Vector3 offset = closestShipSnapPoint.position - closestPartSnapPoint;
-            return partPosition + offset;
-        }
-        
-        return null;
-    }
-    
-    // Gets snap points at part edges and corners
-    private List<Vector3> GetPartSnapPoints(Vector3 partPosition, BoxCollider2D partCollider) {
-        List<Vector3> snapPoints = new List<Vector3>();
-        Vector2 partSize = partCollider.size;
-        float partHalfWidth = partSize.x * 0.5f;
-        float partHalfHeight = partSize.y * 0.5f;
-        
-        snapPoints.Add(new Vector3(partPosition.x, partPosition.y + partHalfHeight, 0));
-        snapPoints.Add(new Vector3(partPosition.x, partPosition.y - partHalfHeight, 0));
-        snapPoints.Add(new Vector3(partPosition.x - partHalfWidth, partPosition.y, 0));
-        snapPoints.Add(new Vector3(partPosition.x + partHalfWidth, partPosition.y, 0));
-        
-        snapPoints.Add(new Vector3(partPosition.x - partHalfWidth, partPosition.y + partHalfHeight, 0));
-        snapPoints.Add(new Vector3(partPosition.x + partHalfWidth, partPosition.y + partHalfHeight, 0));
-        snapPoints.Add(new Vector3(partPosition.x - partHalfWidth, partPosition.y - partHalfHeight, 0));
-        snapPoints.Add(new Vector3(partPosition.x + partHalfWidth, partPosition.y - partHalfHeight, 0));
-        
-        return snapPoints;
-    }
-    
-    // Attaches part to ship as a child
-    public void AttachPartToShip(GameObject part) {
-        if (spaceship == null || part == null) return;
-        
-        if (!attachedParts.Contains(part)) {
-            attachedParts.Add(part);
-            Vector3 worldPosition = part.transform.position;
-            part.transform.SetParent(spaceship.transform);
-            part.transform.position = worldPosition;
-        }
-    }
-    
-    // Checks if position overlaps with ship
-    public bool WouldOverlapWithShip(Vector3 position, BoxCollider2D partCollider) {
-        if (spaceshipCollider == null || partCollider == null) return false;
-        
-        Bounds shipBounds = spaceshipCollider.bounds;
-        Bounds partBounds = new Bounds(position, partCollider.bounds.size);
-        
-        return shipBounds.Intersects(partBounds);
     }
 }
