4974fc3e5b5ce9bd7f0bbd53810e05a7db8ce396
ChocolateMilk71 <olliepurdy@comcast.net>
2025-12-26 23:57:48 -0800

    Updated PartDrag.cs to only move a part if the part fits on the ship there

--- Assets/Scripts/PartDrag.cs
+++ Assets/Scripts/PartDrag.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 
-public class PartDrag : MonoBehaviour
-{
-
-private Vector3 screenPoint;
-private Vector3 offset;
-private BoxCollider2D partCollider;
-private Quaternion lockedRotation;
+public class PartDrag : MonoBehaviour {
+    [SerializeField] private GameObject selectedObject;
+    
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Vector3 originalPosition;
+    private Collider2D partCollider;
+    private Quaternion lockedRotation;
+    private ShipBuildingGrid shipGrid;
 
     private void Awake() {
-        partCollider = GetComponent<BoxCollider2D>();
+        partCollider = GetComponent<Collider2D>();
+        shipGrid = ShipBuildingGrid.instance;
         lockedRotation = transform.rotation;
     }
 
     void OnMouseDown() {
+        originalPosition = transform.position;
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        
+        shipGrid.SetGridCellValueByUnityPosition(originalPosition, -1);
     }
 
     void OnMouseDrag() {
@@ -28,29 +34,29 @@ private Quaternion lockedRotation;
         transform.rotation = lockedRotation;
     }
 
-    void OnMouseUp()
-    {
+    void OnMouseUp() {
         transform.rotation = lockedRotation;
         
-        if (ShipBuildingGrid.instance == null || partCollider == null) return;
+        if (shipGrid == null || partCollider == null) return;
         
-        Vector3? shipSnapPosition = ShipBuildingGrid.instance.SnapToShipSurface(transform.position, partCollider);
-        
-        if (shipSnapPosition != null) {
-            transform.position = (Vector3)shipSnapPosition;
-            ShipBuildingGrid.instance.AttachPartToShip(gameObject);
+        Vector3? nullableGridSnapPosition = ShipBuildingGrid.instance.PostionToGridPosition(transform.position);
+        if (nullableGridSnapPosition == null) return;
+        Vector3 gridSnapPosition = (Vector3)nullableGridSnapPosition;
+
+        GameObject part = SpacecraftPartDatabase.Instance.GetPartGameObjectByName(selectedObject.name);
+        int partID = SpacecraftPartDatabase.Instance.GetPartID(part);
+
+        if (!shipGrid.CanPlacePart(part, shipGrid.UnityPositionToGridCoordinates(gridSnapPosition))) {
+            transform.position = originalPosition;
+            shipGrid.SetGridCellValueByUnityPosition(originalPosition, partID);
             return;
         }
         
-        Vector3? gridSnapPosition = ShipBuildingGrid.instance.PostionToGridPosition(transform.position);
-        if (gridSnapPosition != null) {
-            transform.position = (Vector3)gridSnapPosition;
-        }
+        transform.position = gridSnapPosition;
+        shipGrid.SetGridCellValueByUnityPosition(transform.position, partID);
     }
     
     private void Update() {
-        if (transform.rotation != lockedRotation) {
-            transform.rotation = lockedRotation;
-        }
+        if (transform.rotation != lockedRotation) transform.rotation = lockedRotation;
     }
 }
