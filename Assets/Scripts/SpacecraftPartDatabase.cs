5fe674dbcd3f88f934a437a555f315762bc5c027
ChocolateMilk71 <olliepurdy@comcast.net>
2025-12-27 00:11:40 -0800

    Create SpacecraftPartDatabase.cs
    This database Holds an array of all parts as scriptable objects. See PartScriptableObject.cs commit for info on the different parts of the SO. Allows for easy conversion and obtainability of parts and their connectable directions via either their name or ID.

---
+++ Assets/Scripts/SpacecraftPartDatabase.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpacecraftPartDatabase : MonoBehaviour {
+    [SerializeField] private PartScriptableObject[] allParts;
+
+    public static SpacecraftPartDatabase Instance;
+
+    public void Awake() {
+        Instance = this;
+    }
+    
+    public int GetPartID(GameObject part) {
+        foreach (PartScriptableObject partSO in allParts) {
+            if (partSO.part == part) return partSO.partID;
+        }
+
+        return -1;
+    }
+    
+    public GameObject GetPartGameObject(int id) {
+        foreach (PartScriptableObject partSO in allParts) {
+            if (partSO.partID == id) return partSO.part;
+        }
+
+        return null;
+    }
+
+    public GameObject GetPartGameObjectByName(string objectOrObjectCloneName) {
+        if (objectOrObjectCloneName.Contains("(Clone)")) {
+            string nameWithoutClone = "";
+
+            foreach (char c in objectOrObjectCloneName) {
+                if (c == '(') break;
+
+                nameWithoutClone += c;
+            }
+
+            objectOrObjectCloneName = nameWithoutClone;
+        }
+        
+        foreach (PartScriptableObject partSO in allParts) {
+            if (partSO.part.name == objectOrObjectCloneName) return partSO.part;
+        }
+
+        return null;
+    }
+
+    public List<string> GetSnapableDirections(GameObject part) => GetSnapableDirections(GetPartID(part));
+
+    public List<string> GetSnapableDirections(int id) {
+        foreach (PartScriptableObject partSO in allParts) {
+            if (partSO.partID == id) {
+                return partSO.connectingDirections.ToList();
+            }
+        }
+
+        return null;
+    }
+}
