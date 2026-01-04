bffd1111a763782892242adef61814e9830cabc7
ChocolateMilk71 <olliepurdy@comcast.net>
2026-01-03 17:49:41 -0800

    Update GameInput.cs to allow you to switch scenes with tab

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class GameInput : MonoBehaviour {
     public event EventHandler<EngineActivatedEventArgs> OnActivateEnginePerformedAction;
@@ -12,22 +13,24 @@ public class GameInput : MonoBehaviour {
     
     private InputSystem_Actions inputActions;
     
-    public class EngineActivatedEventArgs : EventArgs {
+     public class EngineActivatedEventArgs : EventArgs {
         public bool activated;
-
+    
         public EngineActivatedEventArgs(bool activated) => this.activated = activated;
     }
     
     public class NumKeyEventArgs : EventArgs {
         public int key;
-
+    
         public NumKeyEventArgs(int key) => this.key = key;
     }
     
     public void Awake() {
         inputActions = new InputSystem_Actions();
-
+    
         inputActions.Spacecraft.Enable();
+        inputActions.General.Enable();
+        
         inputActions.Spacecraft.ActivateEngine.performed += ActivateEngine_performed;
         inputActions.Spacecraft.ActivateEngine.canceled += ActivateEngine_canceled;
         
@@ -39,10 +42,10 @@ public class GameInput : MonoBehaviour {
         inputActions.Spacecraft.KeySix.performed += KeySix_performed;
         inputActions.Spacecraft.KeySeven.performed += KeySeven_performed;
         inputActions.Spacecraft.KeyEight.performed += KeyEight_performed;
-
-
-
+        
         inputActions.Spacecraft.LeftMouseClick.performed += LeftMouseClick_performed;
+    
+        inputActions.General.SceneSwitch.performed += SceneSwitch_performed;
     }
     
     private void ActivateEngine_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
@@ -53,45 +56,51 @@ public class GameInput : MonoBehaviour {
         OnActivateEngineCanceledAction?.Invoke(this, new EngineActivatedEventArgs(false)); 
     }
     
-    private void KeyOne_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+    private void KeyOne_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) { 
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(1)); 
     }
     
     private void KeyTwo_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(2)); 
     }
-
+    
     private void KeyThree_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(3));
     }
-
+    
     private void KeyFour_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(4));
     }
-
+    
     private void KeyFive_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(5));
     }
-
+    
     private void KeySix_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(6));
     }
-
+    
     private void KeySeven_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(7));
     }
-
+    
     private void KeyEight_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         OnNumKeyPerformedAction?.Invoke(this, new NumKeyEventArgs(8));
     }
+    
     private void LeftMouseClick_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
         OnLeftMouseClickPerformedAction?.Invoke(this, EventArgs.Empty); 
+    }
+    
+    private void SceneSwitch_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        if (SceneManager.GetActiveScene().name == "FlightScene") SceneManager.LoadScene("BuildScene");
+        else SceneManager.LoadScene("FlightScene");
     }
     
     private void OnDisable() {
@@ -105,7 +114,7 @@ public class GameInput : MonoBehaviour {
     }
     
     private void CleanupInputActions() {
-        // Properly disable and cleanup input actions
+        //Properly disable and cleanup input actions
         if (inputActions != null) {
             // Unsubscribe from all events first
             inputActions.Spacecraft.ActivateEngine.performed -= ActivateEngine_performed;
