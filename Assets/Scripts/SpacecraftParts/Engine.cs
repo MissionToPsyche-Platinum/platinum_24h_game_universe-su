2644983cde031da86c6e47e66427d6f0714b2915
bananacat50 <elllliotttt@gmail.com>
2026-01-18 06:06:12 -0800

    Made engines provide thrust only when the OnActivateEnginePerformedAction event is triggered
    note: this force is not visible unless the rigidjoints are removed, as they prevent movement

--- Assets/Scripts/SpacecraftParts/Engine.cs
+++ Assets/Scripts/SpacecraftParts/Engine.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class Engine : MonoBehaviour {
     [SerializeField] private int speed;
+    private GameInput gameInput;
     private Rigidbody2D engineRigidbody2D;
+    private static bool active;
 
     public void Awake()
     {
         enabled = false;
         engineRigidbody2D =  GetComponentInParent<Rigidbody2D>();
+        gameInput = GameInput.instance;
+        gameInput.OnActivateEnginePerformedAction += GameInput_OnActivateEngineAction; //Adds GameInput_OnActivateEngineAction() as a listener to the OnActivateEngineAction event. 
+        gameInput.OnActivateEngineCanceledAction += GameInput_OnActivateEngineAction;
     }
-
-    public void Start()
-    {
-    }
-    
     private void FixedUpdate()
     {
-        engineRigidbody2D.AddForce(speed * transform.up * Time.deltaTime);
-    } 
+        if (active)
+        {
+            engineRigidbody2D.AddForce(speed * transform.up * Time.deltaTime);
+        }
+    }
+
+    private void GameInput_OnActivateEngineAction(object sender, GameInput.EngineActivatedEventArgs e) { 
+        active = e.activated;
+    }
 }
