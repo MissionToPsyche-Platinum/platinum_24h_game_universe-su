1dcb6310e7a8334b566ce1bee00d9020a1f6bbf8
Timothylee23 <tlee8@seattleu.edu>
2026-01-20 22:07:48 -0800

    Added Fuel system fo Engine.cs with a max fuel that takes a amount of fuel every second

--- Assets/Scripts/SpacecraftParts/Engine.cs
+++ Assets/Scripts/SpacecraftParts/Engine.cs
@@ -1,29 +1,56 @@
 using UnityEngine;
 
 //Class defines the behavior of the engine part. 
-public class Engine : MonoBehaviour {
+public class Engine : MonoBehaviour
+{
     [SerializeField] private int speed;
     private GameInput gameInput;
     private Rigidbody2D engineRigidbody2D;
     private static bool active;
+    private float fuelAmount = 10f; // Fuel amount
 
     public void Awake()
     {
         enabled = false;
-        engineRigidbody2D =  GetComponentInParent<Rigidbody2D>();
+        engineRigidbody2D = GetComponentInParent<Rigidbody2D>();
         gameInput = GameInput.instance;
         gameInput.OnActivateEnginePerformedAction += GameInput_OnActivateEngineAction; //Adds GameInput_OnActivateEngineAction() as a listener to the OnActivateEngineAction event. 
         gameInput.OnActivateEngineCanceledAction += GameInput_OnActivateEngineAction;
     }
     private void FixedUpdate()
     {
-        if (active)
+        if (!active)
         {
-            engineRigidbody2D.AddForce(speed * transform.up * Time.deltaTime);
+            return;
         }
+
+
+        Debug.Log(fuelAmount); // Log current fuel amount for testing
+
+        // Check and see if we have fuel
+        if (fuelAmount <= 0f)
+        {
+            fuelAmount = 0f;
+            return; // no fuel so no thrust
+        }
+
+        // Apply thrust
+        engineRigidbody2D.AddForce(
+            speed * transform.up * Time.fixedDeltaTime
+        );
+
+        // Consume fuel AFTER thrust
+        ConsumeFuel();
     }
 
-    private void GameInput_OnActivateEngineAction(object sender, GameInput.EngineActivatedEventArgs e) { 
+    private void GameInput_OnActivateEngineAction(object sender, GameInput.EngineActivatedEventArgs e)
+    {
         active = e.activated;
+    }
+
+    private void ConsumeFuel()
+    {
+        float fuelConsumptionRate = 1f; // consumption rate
+        fuelAmount -= fuelConsumptionRate * Time.deltaTime;
     }
 }
