521b5d66b5b903c776807d816de1f911b3826a9c
Timothylee23 <tlee8@seattleu.edu>
2026-04-09 14:44:16 -0700

    Revert "Created the ability to zoom in and out via the scroll wheel"
    This reverts commit b39030f298ec109670c6abf59991e772111c2f08.

--- Assets/Scripts/FlightScene/AsteroidController.cs
+++ Assets/Scripts/FlightScene/AsteroidController.cs
@@ -27,6 +27,8 @@ public class AsteroidController : MonoBehaviour {
     private Dictionary<GameObject, float> outOfCameraTimes = new();
     private void Awake() {
         Instance = this;
+        maxHorizontalSpawnRange = (camera.GetComponent<FlightCamera>().GetCameraWidth() / 2) + distanceFromCameraBorder;
+        maxVerticalSpawnRange = (camera.GetComponent<FlightCamera>().GetCameraHeight() / 2) + distanceFromCameraBorder;
     }
 
     private void Start() {
@@ -35,22 +37,10 @@ public class AsteroidController : MonoBehaviour {
     
     private void Update() {
         foreach (GameObject asteroid in outOfCameraTimes.Keys.ToList()) {
-            if (asteroid == null) continue;
-
-            // Check if asteroid is currently visible in camera
-            Vector3 viewportPos = camera.WorldToViewportPoint(asteroid.transform.position);
-            bool isVisible = viewportPos.x >= 0 && viewportPos.x <= 1 &&
-                             viewportPos.y >= 0 && viewportPos.y <= 1;
-
-            if (isVisible) {
-                outOfCameraTimes[asteroid] = 0f; // reset timer if back in view
-            }
-            else {
-                outOfCameraTimes[asteroid] += Time.deltaTime;
-                if (outOfCameraTimes[asteroid] >= 3f) DestroyAsteroid(asteroid);
-            }
+            outOfCameraTimes[asteroid] += Time.deltaTime;
+            if(outOfCameraTimes[asteroid] >= 3f) DestroyAsteroid(asteroid);
         }
-
+        
         if (currentAsteroidCount == maxAsteroids) return;
 
         timeUntilNextAsteroidSpawn -= Time.deltaTime;
@@ -106,28 +96,24 @@ public class AsteroidController : MonoBehaviour {
     }
 
     private Vector3 GetSpawnPosition(out int spawnSide) {
-        FlightCamera flightCamera = camera.GetComponent<FlightCamera>();
-        float maxHorizontalSpawnRange = (flightCamera.GetCameraWidth() / 2) + distanceFromCameraBorder;
-        float maxVerticalSpawnRange = (flightCamera.GetCameraHeight() / 2) + distanceFromCameraBorder;
-
         spawnSide = UnityEngine.Random.Range(0, 4);
         Vector3 offset = new Vector3();
-
+        
         switch (spawnSide) {
-            case 0:
+            case 0: //Spawn above camera
                 offset = new Vector3(UnityEngine.Random.Range(-maxHorizontalSpawnRange, maxHorizontalSpawnRange), maxVerticalSpawnRange, -camera.transform.position.z);
                 break;
-            case 1:
+            case 1: //below
                 offset = new Vector3(UnityEngine.Random.Range(-maxHorizontalSpawnRange, maxHorizontalSpawnRange), -maxVerticalSpawnRange, -camera.transform.position.z);
                 break;
-            case 2:
+            case 2: //left
                 offset = new Vector3(-maxHorizontalSpawnRange, UnityEngine.Random.Range(-maxVerticalSpawnRange, maxVerticalSpawnRange), -camera.transform.position.z);
                 break;
-            case 3:
+            case 3: //right
                 offset = new Vector3(maxHorizontalSpawnRange, UnityEngine.Random.Range(-maxVerticalSpawnRange, maxVerticalSpawnRange), -camera.transform.position.z);
                 break;
         }
-
+        
         return camera.transform.position + offset;
     }
 
